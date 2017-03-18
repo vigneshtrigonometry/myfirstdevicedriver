1d29545dbf4312046e036f842560234cd0430274
Vignesh <vigneshtrigonometry@gmail.com>
2017-03-18 19:51:19 +0800

    Read and write implemented

--- mydriver.c
+++ mydriver.c
@@ -13,22 +13,18 @@
 /* forward declaration */
 int onebyte_open(struct inode *inode, struct file *filep);
 int onebyte_release(struct inode *inode, struct file *filep);
-ssize_t onebyte_read(struct file *filep, char *buf, size_t
-count, loff_t *f_pos);
-ssize_t onebyte_write(struct file *filep, const char *buf,
-size_t count, loff_t *f_pos);
+ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos);
+ssize_t onebyte_write(struct file *filep, const char *buf,size_t count, loff_t *f_pos);
 static void onebyte_exit(void);
 
 /* definition of file_operation structure */
 struct file_operations onebyte_fops = {
-read:
-onebyte_read,
-write:
-onebyte_write,
-open:
-onebyte_open,
+read: onebyte_read,
+write: onebyte_write,
+open: onebyte_open,
 release: onebyte_release
 };
+
 char *onebyte_data = NULL;
 
 int onebyte_open(struct inode *inode, struct file *filep)
@@ -44,11 +40,29 @@ return 0; // always successful
 ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 {
 /*please complete the function on your own*/
+//linux doc func to output data to user space
+//unsigned long copy_to_user (void __user *to,const void *from,unsigned long n);
+if(onebyte_data != NULL){
+copy_to_user(buf, onebyte_data, 1);
+return 1; //just read one byte
+}
+return 0; // if my data is null then don't read anything
+
 }
 
 ssize_t onebyte_write(struct file *filep, const char *buf,size_t count, loff_t *f_pos)
 {
 /*please complete the function on your own*/
+/* loff_t is long offset - a kernel data structure that points to offset provided by user.
+This must be updated because it tells us where to write. (Seek pointers) */
+if(*f_pos ==0 ){
+copy_from_user(onebyte_data, buf, 1);
+*f_pos += 1;
+return 1;
+}
+else{
+return -ENOSPC;
+}
 }
 
 static int onebyte_init(void)
